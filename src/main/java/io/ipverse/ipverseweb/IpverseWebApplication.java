3a5d8ecf5005cc6cf442d2bb351c83a999e28a02
planetdoy <49358574+planetdoy@users.noreply.github.com>
2021-04-26 11:32:29 +0900

    project setting commit

---
+++ src/main/java/io/ipverse/ipverseweb/IpverseWebApplication.java
@@ -0,0 +1,11 @@
+package io.ipverse.ipverseweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpverseWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IpverseWebApplication.class, args);
+	}
+}
